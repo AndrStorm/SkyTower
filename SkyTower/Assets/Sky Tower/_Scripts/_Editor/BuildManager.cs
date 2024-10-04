b3404b4c42f723d4bf6a9759832046595cb81ab0
AndrStorm <f12.2@yandex.ru>
2024-10-04 18:53:37 +0300

    Rename Core Folder

---
+++ SkyTower/Assets/Sky Tower/_Scripts/_Editor/BuildManager.cs
@@ -0,0 +1,197 @@
+#if UNITY_EDITOR
+
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using LootLocker;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+using GameAnalyticsSDK;
+
+public class BuildManager : IPreprocessBuildWithReport, IPostprocessBuildWithReport
+{
+
+    public int callbackOrder => 0;
+
+    public void OnPostprocessBuild(BuildReport report)
+    {
+        PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, "com.AndrStormGames.SkyTower");
+    }
+
+    public void OnPreprocessBuild(BuildReport report)
+    {
+#if TEST_BUILD
+       Debug.Log("Test Build");
+        
+#elif AG_BUILD
+
+    #if RU_VERSION
+        Debug.Log("App Gallery ru");
+        PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, "com.AndrStormGames.SkyTower.huawei.ru");
+        
+    #elif EN_VERSION
+        Debug.Log("App Gallery en");
+        PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, "com.AndrStormGames.SkyTower.huawei");
+        
+    #else
+        Debug.Log("App Gallery default");
+        PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, "com.AndrStormGames.SkyTower.huawei");
+        
+    #endif
+        
+#elif RS_BUILD
+        Debug.Log("Ru Store ");
+        PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, "com.AndrStormGames.SkyTower");
+        
+#elif GP_BUILD
+        Debug.Log("Google Play");
+        PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, "com.AndrStormGames.SkyTower");
+#endif
+    }
+    
+}
+
+
+[InitializeOnLoad]
+public class DefininitionsManager : Editor
+{
+    
+    private static readonly string [] DefineKeywords = {
+        "UNITY_POST_PROCESSING_STACK_V2",
+        //"TEST_BUILD",
+        "GP_BUILD",
+        //"AG_BUILD",
+        //"RS_BUILD",
+        //"RU_VERSION",
+        "EN_VERSION",
+    };
+    
+    
+    static DefininitionsManager ()
+    {
+        SetUpDefinitions();
+        
+        string bundleVersion = PlayerSettings.bundleVersion;
+        SetUpGameAnalytics(bundleVersion);
+        LocaleDefininitionsManager.SetUp();
+        LootLockerDefenitionsManager.SetUp(bundleVersion);
+    }
+
+    private static void SetUpDefinitions()
+    {
+        List<string> allDefines = new List<string>();
+        allDefines.AddRange(DefineKeywords.Except(allDefines));
+
+        string definitions = string.Join(";", allDefines.ToArray());
+        Debug.Log(definitions);
+        
+        PlayerSettings.SetScriptingDefineSymbolsForGroup
+            (EditorUserBuildSettings.selectedBuildTargetGroup, definitions);
+    }
+    
+    //CustomEditor
+    /*
+     private static string[] oldDefines;
+    private static void AddDefineSymbols ()
+    {
+        List<string> allDefines = new List<string>();
+        allDefines.AddRange(DefineKeywords.Except(allDefines));
+        PlayerSettings.SetScriptingDefineSymbolsForGroup(
+            EditorUserBuildSettings.selectedBuildTargetGroup,
+            string.Join(";", allDefines.ToArray()));
+        
+        Debug.Log("defines " + PlayerSettings.GetScriptingDefineSymbols(NamedBuildTarget.Android));
+    }
+
+    private void OnEnable()
+    {
+        using (var check = new EditorGUI.ChangeCheckScope())
+        {
+            Debug.Log("Defines Check");
+            oldDefines = DefineKeywords; 
+            if (check.changed)
+            {
+                AddDefineSymbols();
+                Debug.Log("Defines Changed");
+            }
+            else
+            {
+                Debug.Log("Defines Checked");
+            }
+        }
+    }
+    */
+
+    
+    private static void SetUpGameAnalytics(string bundleVersion)
+    {
+        var buildVersion = new List<string>(){bundleVersion};
+        GameAnalytics.SettingsGA.Build = buildVersion;
+    }
+
+    
+    private static class LocaleDefininitionsManager 
+    {
+    
+        public static void SetUp()
+        {
+            int i = -1;
+            var specificSelectorsList = new List<int>();
+            foreach (var selector in LocalizationSettings.StartupLocaleSelectors)
+            {
+                i++;
+                if (selector.GetType() == typeof(SpecificLocaleSelector))
+                {
+                    specificSelectorsList.Add(i);
+                }
+            }
+
+#if RU_VERSION
+            LocaleIdentifier identifier = new LocaleIdentifier(new CultureInfo("ru"));
+#else
+            LocaleIdentifier identifier = new LocaleIdentifier(new CultureInfo("en"));      
+#endif
+        
+            SpecificLocaleSelector newSelector = new SpecificLocaleSelector
+            {
+                LocaleId = identifier
+            };
+        
+            LocalizationSettings.StartupLocaleSelectors.Add(newSelector);
+
+            foreach (var selectorID in specificSelectorsList)
+            {
+                LocalizationSettings.StartupLocaleSelectors.RemoveAt(selectorID);
+            }
+        }
+
+    }
+    
+    private static class LootLockerDefenitionsManager
+    {
+        private const string DEVELOP_MOD_API_KEY = "dev_cc3723a2ab584e9bb391faaf3ef1445d";
+        //dev_cc3723a2ab584e9bb391faaf3ef1445d
+        private const string LIVE_MOD_API_KEY = "prod_3d88b362e4ea45a68c3251216574fcad"; 
+        //prod_3d88b362e4ea45a68c3251216574fcad
+
+
+        public static void SetUp(string bundleVersion)
+        {
+            var config = LootLockerConfig.Get();
+            config.game_version = bundleVersion;
+        
+#if TEST_BUILD
+            config.apiKey = DEVELOP_MOD_API_KEY;
+            config.developmentMode = true;
+#else
+            config.apiKey = LIVE_MOD_API_KEY;
+            config.developmentMode = false;
+#endif
+        }
+    }
+}
+#endif
---
+++ SkyTower/Assets/Sky Tower/_Scripts/_Main/_Managers/UnityAdsManager.cs
@@ -0,0 +1,214 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class UnityAdsManager : Singleton<UnityAdsManager>, IUnityAdsInitializationListener,
+    IUnityAdsLoadListener, IUnityAdsShowListener
+{
+    
+    [SerializeField] private string _androidGameId = "4618933";
+    [SerializeField] private string _iOSGameId = "4618932";
+    [SerializeField] private bool _testMode = true;
+    
+    [SerializeField] private BannerPosition _bannerPosition = BannerPosition.TOP_CENTER;
+
+    [SerializeField] private string _interstitial_Android = "Interstitial_Android";
+    [SerializeField] private string _interstitial_iOS = "Interstitial_iOS";
+    [SerializeField] private string _banner_Android = "Banner_Android";
+    [SerializeField] private string _banner_iOS = "Banner_iOS";
+    [SerializeField] private string _rewarded_Android = "Rewarded_Android";
+    [SerializeField] private string _rewarded_iOS = "Rewarded_iOS";
+    
+    [SerializeField] private int _restartsToShowAds = 3;
+    
+    public int RestartsToShowAds => _restartsToShowAds;
+
+    
+    private string _gameId;
+
+    
+    
+    private void Start()
+    {
+        //обработка ошибок, не работает
+        //так как обработка происходит в сервисе через евент
+        // try
+        // {
+        //     InitializeAds();
+        // }
+        // catch (Exception e)
+        // {
+        //     Helper.Log(e.Message);
+        // }
+        InitializeAds();
+    }
+    
+    private void InitializeAds()
+    {
+        _gameId = IsiOS() ? _iOSGameId : _androidGameId;
+        
+        SetUpPersonolizedAds();
+        Advertisement.Initialize(_gameId, _testMode, Instance);
+        Advertisement.Banner.SetPosition(_bannerPosition);
+    }
+
+    private void SetUpPersonolizedAds()
+    {
+#if GP_BUILD
+        // If the user opts out of personalized ads:
+        MetaData userMetaData = new MetaData("user");
+        userMetaData.Set("nonbehavioral", "true");
+        Advertisement.SetMetaData(userMetaData);
+#else
+        MetaData userMetaData = new MetaData("user");
+        userMetaData.Set("nonbehavioral", "false");
+        Advertisement.SetMetaData(userMetaData);
+#endif
+        
+    }
+
+    private bool IsiOS()
+    {
+        return Application.platform == RuntimePlatform.IPhonePlayer;
+    }
+
+    
+    
+    
+    
+    public void ShowFullScreenAds(string placementId)
+    {
+        StartCoroutine(ShowFullScreenAdsCoroutine(placementId));
+    }
+    
+    private IEnumerator ShowFullScreenAdsCoroutine(string placementId)
+    {
+        if (!Advertisement.isInitialized)
+        {
+            yield return Helper.GetUnscaledWait(0.5f);
+            //Debug.Log("banner is not ready");
+        }
+        Advertisement.Load(placementId, Instance);
+    }
+    
+    
+    public void ShowBannerAds()
+    {
+        StartCoroutine(ShowBannerCoroutine());
+    }
+    
+    private IEnumerator ShowBannerCoroutine()
+    {
+        if (!Advertisement.isInitialized)
+        {
+            yield return Helper.GetUnscaledWait(0.5f);
+            //Debug.Log("banner is not ready");
+        }
+        
+        Advertisement.Banner.SetPosition(_bannerPosition);
+        Advertisement.Banner.Load(GetBannerId());
+        
+        if (!Advertisement.Banner.isLoaded)
+        {
+            yield return Helper.GetUnscaledWait(0.5f);
+            //Debug.Log("banner is not loaded"); 
+        }
+        
+        Advertisement.Banner.Show(GetBannerId());
+        //Debug.Log("GetBannerId() " + GetBannerId());
+    }
+    
+    private string GetBannerId()
+    {
+        return IsiOS() ? _banner_iOS : _banner_Android;;
+    }
+    
+    public void HideBannerAds()
+    {
+        Advertisement.Banner.Hide();
+    }
+    
+    
+    public string GetInterstitialId()
+    {
+        return IsiOS() ? _interstitial_iOS : _interstitial_Android;;
+    }
+    
+    public string GetRewardedId()
+    {
+        return IsiOS() ? _rewarded_iOS : _rewarded_Android;;
+    }
+    
+
+    
+    
+    
+    private void OnAdsLoaded(string placementId) 
+    {
+        Advertisement.Show(placementId, Instance);
+        //Debug.Log("OnAdsLoaded " + placementId);
+    }
+
+    
+    public void OnInitializationComplete()
+    {
+        //Debug.Log("Unity Ads initialization complete.");
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        Debug.Log($"Unity Ads Initialization Failed: {error.ToString()} - {message}");
+    }
+
+
+    public void OnUnityAdsAdLoaded(string placementId)
+    {
+        OnAdsLoaded(placementId);
+    }
+
+    public void OnUnityAdsFailedToLoad(string placementId, UnityAdsLoadError error, string message)
+    {
+        Debug.Log($"OnUnityAdsFailedToLoad placementId {placementId} error {error}");
+    }
+    
+
+    public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
+    {
+        Debug.Log($"OnUnityAdsShowFailure placementId {placementId} error {error}");
+    }
+
+    public void OnUnityAdsShowStart(string placementId)
+    {
+        //Debug.Log($"OnUnityAdsShowStart placementId {placementId}");
+    }
+
+    public void OnUnityAdsShowClick(string placementId)
+    {
+        //Debug.Log($"OnUnityAdsShowClick placementId {placementId}");
+    }
+
+    public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
+    {
+        //Debug.Log($"OnUnityAdsShowComplete placementId {placementId}");
+        switch (showCompletionState)
+        {
+            case UnityAdsShowCompletionState.SKIPPED:
+                Debug.Log($"SKIPPED");
+                break;
+            
+            case UnityAdsShowCompletionState.COMPLETED:
+                Debug.Log($"COMPLETED");
+                break;
+            
+            case UnityAdsShowCompletionState.UNKNOWN:
+                Debug.Log($"UNKNOWN");
+                break;
+            
+            default:
+                Debug.Log($"UNKNOWN");
+                break;
+
+        }
+    }
+}
